51387f28a94909b6c98e0c4c54f6588ac36d1146
rajatgupta310198 <rajatgupta310198@gmail.com>
2016-10-13 01:18:04 +0530

    Initial registration backend  added

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
-
+from django.contrib.auth import login,logout,authenticate
 
 def index(request):
     return render(request,'index.html')
@@ -11,5 +11,17 @@ def join(request):
     return render(request,'join.html')
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+
+        user = User(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+        user.save()
+        u = authenticate(username=username,password=password)
+        login(request,u)
+        return render(request,'dashboard.html',{'u':u})
+
 
