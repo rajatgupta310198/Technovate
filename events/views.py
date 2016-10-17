d79d9ff12f123b5e5e7b8f44fbe4e21d3429e318
Rajat Gupta <rajat310198@outlook.com>
2016-10-17 19:38:47 +0530

    Update views.py

--- events/views.py
+++ events/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 # Create your views here.
 from django.contrib.auth import login,logout,authenticate
 from events.models import Events,Profile
+
+
 def index(request):
     return render(request,'index.html')
 
@@ -11,26 +13,25 @@ def index(request):
 def join(request):
     return render(request,'join.html')
 
+
 def dashboard(request):
     return render(request,'dashboard.html')
 
+
 def register(request):
     if request.method == 'POST':
-        username = request.POST['username']
         password = request.POST['password']
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
-        user = User.objects.create_user(username=username,password=password,email=email,first_name=first_name,last_name=last_name)
+        user = User.objects.create_user(username=email,password=password,email=email,first_name=first_name,last_name=last_name)
         user.set_password(password)
         user.save()
-        U = authenticate(username=username,password=password)
+        U = authenticate(username=email,password=password)
         if U is not None:
             login(request,U)
             events = Events.objects.all()
             return render(request,'dashboard.html',{'events':events})
-
-
 
 
 def UpdateAndCreateProfile(request):
@@ -41,8 +42,8 @@ def UpdateAndCreateProfile(request):
         p.Institute_Uni = request.POST['institute_uni']
         p.PhoneNo = request.POST['phoneno']
         p.save()
-        e = Events.objects.filter(pk=request.POST['event'])
-        p.events.add()
+        # e = Events.objects.filter(pk=request.POST['event'])
+        #p.events.add()
         p.save()
         p.user.add(U)
         return render(request,'index.html')
@@ -55,7 +56,7 @@ def events(request):
     
 def LOG_IN(request):
     if request.method == "POST":
-        username = request.POST['username']
+        username = request.POST['email']
         password = request.POST['password']
         user = authenticate(username=username,password=password)
         if user is not None:
