f320338f32db0d7c6b3fc9e7bb8337131583c635
Rajat Gupta <rajat310198@outlook.com>
2017-01-02 23:11:59 +0530

    Update admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from events.models import Events,Profile
+from events.models import Events,Profile,CampusRepresantative
 # Register your models here.
 
 
 admin.site.register(Events)
 admin.site.register(Profile)
-
+admin.site.register(CampusRepresantative)
