7183d16a55dfe58c953d2d225f1f1acd4aa1c66b
Rajat Gupta <rajat310198@outlook.com>
2017-01-24 00:44:26 +0530

    Update admin.py

--- events/admin.py
+++ events/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from events.models import Events,Profile,CampusRepresantative
 # Register your models here.
 
+class EventAdmin(admin.ModelAdmin):
+	model = Events
+	list_display  = ('EventName' ,'EventDescription','eventCost')
 
-admin.site.register(Events)
-admin.site.register(Profile)
-admin.site.register(CampusRepresantative)
+class ProfileAdmin(admin.ModelAdmin):
+	model = Profile
+	list_display = ('Name','PhoneNo','Institute_Uni')
+
+class CampusRepresentativeAdmmin(admin.ModelAdmin):
+	model = CampusRepresantative
+	list_display = ('Name','Phone','email','Institute')
+
+admin.site.register(Events,EventAdmin)
+admin.site.register(Profile,ProfileAdmin)
+admin.site.register(CampusRepresantative,CampusRepresentativeAdmmin)	
